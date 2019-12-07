c039f716b12294159e326874fa31ff54812f30b6
GeekCC <1352290940@qq.com>
2019-12-07 12:17:34 +0800

    完整版

--- part1/part1_4.py
+++ part1/part1_4.py
@@ -1,63 +1,35 @@
-# def LCS(strA,strB):
-#     lenA = len(strA)
-#     lenB = len(strB)
-#     out = []    
-#     N = [[0]*(lenB+1) for i in range(lenA+1)]
-#     H = [[0]*(lenB+1) for i in range(lenA+1)]
-#     for i in range(1,lenA+1):
-#         for j in range(1,lenB+1):
-#             if strA[i-1] == strB[j-1]:
-#                 N[i][j] = N[i-1][j-1]+1
-#                 H[i][j] = 1
-#                 break
-#             elif N[i][j-1] >= N[i-1][j]:
-#                 N[i][j] = N[i][j-1]
-#                 H[i][j] = 'n'
-#             else:
-#                 N[i][j] = N[i-1][j]
-#                 H[i][j] = 'top'
-#     print(N)
-#     print(H)
-#     return out
-
-# def main():
-#     strA = input()
-#     strB = input()
-#     print(LCS(strA,strB))
-
-# if __name__=='__main__':
-#     main()
-cnt = 0
 str1='adacbcb'
 str2='abdebcbb'
-num = [[0]*len(str2) for i in range(len(str1))]
-def LCS(str1, str2, index1, index2):
-    global cnt
+num = [[0]*len(str2) for i in range(len(str1))] #一个演示数组
+maxlen = 0
+maxsub = []
+
+def LCS(str1, str2, index1, index2, strlen, substr):
+    global maxlen
+    global maxsub
+    sub = substr.copy()
     if index1 == len(str1) or index2 == len(str2):
         return
     if str1[index1] == str2[index2]:
+        sub.append(str1[index1])
+        strlen += 1
+        if strlen > maxlen:
+            maxlen = strlen
+            maxsub = sub
         num[index1][index2] = 1
-        LCS(str1, str2, index1+1, index2+1)
+        LCS(str1, str2, index1+1, index2+1, strlen, sub)
     else:
-        LCS(str1, str2, index1+1, index2)
-        LCS(str1, str2, index1, index2+1)
+        LCS(str1, str2, index1+1, index2, strlen, sub)
+        LCS(str1, str2, index1, index2+1, strlen, sub)
 
-# str1 = input()
-# str2 = input()
-# def Search(matrix, row, col):
-#     while(col < len(str2)):
-#         if matrix[row][col] == 1:
-#             Search(matrix, row+1， col+1)
-#         col += 1
-
-LCS(str1, str2, 0, 0)
+LCS(str1, str2, 0, 0, 0, substr = [])
 print("   ",end='')
 for i in str2:
     print(i,' ',end='')
-print('\n')
+print()
 for i in range(len(str1)):
     print(str1[i], num[i][:])
 
-
+print("MaxSubStr:",maxsub)
 
 
